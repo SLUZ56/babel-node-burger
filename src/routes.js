cc0002b646f26a203e7b88c98c8fc9f64f13cc63
SLUZ56 <sd54diasinacio@gmail.com>
2023-10-05 17:01:55 -0300

    finished work

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,19 @@
-import { Router } from "express";
+import { Router } from "express"
+import { v4 } from "uuid"
 
-const routes = new Router();
+import User from "./app/models/User"
 
-routes.get("/", (req, res) => {
-  return res.json({ message: "Hello World!" });
-});
+const routes = new Router()
 
-export default routes;
+routes.get("/", async (req, res) => {
+  // const user = await User.create({
+  //   id: v4(),
+  //   name: "Selma",
+  //   email: "selmateste@gmail.com",
+  //   password_hash: "123456",
+  // })
+
+  return res.json()
+})
+
+export default routes
